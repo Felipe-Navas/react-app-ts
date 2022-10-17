e05129c6094ffe9d4b91fe7878144bbda2d49fb0
Felipe Navas <felipenavas.itec@gmail.com>
2022-10-17 08:55:49 -0300

    Adding useShoppingCart

--- src/03-component-patterns/pages/ShoppingPage.tsx
+++ src/03-component-patterns/pages/ShoppingPage.tsx
@@ -4,53 +4,12 @@ import {
   ProductImage,
   ProductTitle,
 } from '../components'
-import { Product } from '../interfaces/interfaces'
+import { products } from '../data/products'
 import '../styles/custom-styles.css'
-import { useState } from 'react'
+import { useShoppingCart } from '../hooks/useShoppingCart'
 
 export const ShoppingPage = () => {
-  const [shoppingCart, setShoppingCart] = useState<{
-    [key: string]: ProductInCart
-  }>({})
-
-  const onProductCountChange = ({
-    count,
-    product,
-  }: {
-    count: number
-    product: Product
-  }) => {
-    setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      }
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        }
-      }
-
-      // Delete the product from the cart
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart
-      return rest
-
-      // Old implementation:
-      // if (count === 0) {
-      // const { [product.id]: toDelete, ...rest } = oldShoppingCart
-
-      // return rest
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count },
-      // }
-    })
-  }
+  const { shoppingCart, onProductCountChange } = useShoppingCart()
 
   return (
     <div>
