bc980339d531593fd61bcf7461d4464d85c389f4
Felipe Navas <felipenavas.itec@gmail.com>
2022-10-18 08:55:55 -0300

    Basic structure for state initializaer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import { Navigation } from './02-lazyload-nested/routes/Navigation'
 
-import { Navigation } from "./03-component-patterns/routes/Navigation"
+import { Navigation } from "./04-component-state-initializer/routes/Navigation"
 
 export const App = () => {
   return (
