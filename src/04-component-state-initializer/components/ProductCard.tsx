05685dfded86b4dc80efdfb9dbaef509e304bceb
Felipe Navas <felipenavas.itec@gmail.com>
2022-10-19 08:54:14 -0300

    Adding initialValues

--- src/04-component-state-initializer/components/ProductCard.tsx
+++ src/04-component-state-initializer/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { createContext } from 'react'
 import { useProduct } from '../hooks/useProduct'
 import {
+  InitialValues,
   onChangeArgs,
   Product,
   ProductContextProps,
@@ -14,21 +15,23 @@ const { Provider } = ProductContext
 export interface Props {
   children?: React.ReactElement | React.ReactElement[]
   className?: string
+  initialValues?: InitialValues
+  onChange?: (args: onChangeArgs) => void
   product: Product
   style?: React.CSSProperties
-  onChange?: (args: onChangeArgs) => void
   value?: number
 }
 
 export const ProductCard = ({
   children,
-  product,
   className,
-  style,
+  initialValues,
   onChange,
+  product,
+  style,
   value,
 }: Props) => {
-  const { counter, increaseBy } = useProduct({ onChange, product, value })
+  const { counter, increaseBy } = useProduct({ onChange, product, value, initialValues })
 
   return (
     <Provider
--- src/04-component-state-initializer/pages/ShoppingPage.tsx
+++ src/04-component-state-initializer/pages/ShoppingPage.tsx
@@ -19,6 +19,10 @@ export const ShoppingPage = () => {
         key={product.id}
         product={product}
         className="bg-dark text-white"
+        initialValues={{
+          count: 4,
+          maxCount: 10
+        }}
       >
         <ProductImage
           className="custom-image"
