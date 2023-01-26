f7c1938aa186afb20c74fc9ba34d12b29cdbdecb
Felipe Navas <felipenavas.itec@gmail.com>
2023-01-26 09:00:30 -0300

    Renaming navigation and working on register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { Navigation } from './02-lazyload-nested/routes/Navigation'
 // import { Navigation } from "./04-component-state-initializer/routes/Navigation"
 
-import { Navigation } from "./05-forms/routes/Navigatin"
+import { Navigation } from "./05-forms/routes/Navigation"
 
 
 export const App = () => {
